6c9636135181d3f2777d88afcee31fb456892bcf
Daniel Scheller <d.scheller@gmx.net>
2012-11-04 13:15:54 +0100

    Complete set for unitymedia channels (info, NVoD)

--- picons.sh
+++ picons.sh
@@ -6448,7 +6448,55 @@ ln -s umpstars.png 1_0_1_C760_96B_2_11A0000_0_0_0.png
 
 # unitymedia
 
+# unitymediainfokanal
+
+# unitymediakinoaufabruf01
+
+# unitymediakinoaufabruf02
+
+# unitymediakinoaufabruf03
+
+# unitymediakinoaufabruf04
+
+# unitymediakinoaufabruf05
+
+# unitymediakinoaufabruf06
+
+# unitymediakinoaufabruf07
+
+# unitymediakinoaufabruf08
+
+# unitymediakinoaufabruf09
+
+# unitymediakinoaufabruf10
+
+# unitymediakinoaufabruf11
+
+# unitymediakinoaufabruf12
+
+# unitymediakinoaufabruf13
+
+# unitymediakinoaufabruf14
+
+# unitymediakinoaufabruf15
+
+# unitymediakinoaufabruf16
+
+# unitymediakinoaufabruf17
+
+# unitymediakinoaufabruf18
+
+# unitymediakinoaufabruf19
+
+# unitymediakinoaufabruf20
+
+# unitymediakinohd
+
+# unitymediakinovorschau
+
 # unitymediaschnupperkanal
+
+# unitymediavideothek
 
 # universalchannel
 ln -s universalchannel.png 1_0_1_2337_C8F_3_EB0000_0_0_0.png
@@ -7755,9 +7803,31 @@ ln -s unitelclassica.png 1_0_1_620F_F1_270F_FFFF0000_0_0_0.png
 ln -s unitelclassica.png 1_0_1_18_11_85_FFFF0000_0_0_0.png
 ln -s unitelclassicahd.png 1_0_19_56B0_E3_A018_FFFF0000_0_0_0.png
 ln -s unitelclassicahd.png 1_0_19_759C_12D_270F_FFFF0000_0_0_0.png
-ln -s unitymedia.png 1_0_1_620D_B5_270F_FFFF0000_0_0_0.png
-ln -s unitymedia.png 1_0_1_3351_83_270F_FFFF0000_0_0_0.png
+ln -s unitymediainfokanal.png 1_0_1_620D_B5_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinovorschau.png 1_0_1_5A49_79_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf01.png 1_0_1_4AA7_6F_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf02.png 1_0_1_4AA6_6F_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf03.png 1_0_1_4AA5_6F_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf04.png 1_0_1_4AA4_6F_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf05.png 1_0_1_4AA3_6F_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf06.png 1_0_1_4AA2_6F_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf07.png 1_0_1_3EEA_79_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf08.png 1_0_1_3EE9_79_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf09.png 1_0_1_3EE8_79_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf10.png 1_0_1_3EE7_79_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf11.png 1_0_1_3EE6_79_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf12.png 1_0_1_3EE5_79_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf13.png 1_0_1_42CD_155_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf14.png 1_0_1_42CE_155_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf15.png 1_0_1_42CF_155_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf16.png 1_0_1_42D0_155_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf17.png 1_0_1_42D1_155_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf18.png 1_0_1_42D2_155_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf19.png 1_0_1_42D3_155_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinoaufabruf20.png 1_0_1_42D4_155_270F_FFFF0000_0_0_0.png
+ln -s unitymediakinohd.png 1_0_19_4E88_155_270F_FFFF0000_0_0_0.png
 ln -s unitymediaschnupperkanal.png 1_0_1_75A1_12D_270F_FFFF0000_0_0_0.png
+ln -s unitymediavideothek.png 1_0_1_3351_83_270F_FFFF0000_0_0_0.png
 ln -s vh1classic.png 1_0_1_3B08_97_270F_FFFF0000_0_0_0.png
 ln -s vh1classic.png 1_0_1_C47E_2715_F001_FFFF0000_0_0_0.png
 ln -s vh1classic.png 1_0_1_CF7B_2715_F001_FFFF0000_0_0_0.png
